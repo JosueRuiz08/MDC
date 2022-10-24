acb50e7f02444ad4284bd3d4c263cdfbabd68c56
JosueRuiz08 <josue.ruiz>
2022-10-24 18:17:48 -0500

    AppBar Bottom

--- app/src/main/java/com/josue/example/mdcomponents/Fragments/AppBarBottomFragment.java
+++ app/src/main/java/com/josue/example/mdcomponents/Fragments/AppBarBottomFragment.java
@@ -2,27 +2,29 @@ package com.josue.example.mdcomponents.Fragments;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.josue.example.mdcomponents.R;
-import com.josue.example.mdcomponents.utils.Component;
-import com.josue.example.mdcomponents.utils.Constants;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AppBarFragment#newInstance} factory method to
+ * Use the {@link AppBarBottomFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AppBarFragment extends Fragment {
+public class AppBarBottomFragment extends DialogFragment {
+
+    public static final String TAG = "AppBarBottomFragment";
+    Unbinder mUnbinder;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,20 +35,7 @@ public class AppBarFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public  static final String TAG = "AppBarFragment";
-    private static Component mInstance;
-    Unbinder mUnbinder;
-
-    public static Component getItemInstance() {
-        mInstance = new Component();
-        mInstance.setName(TAG);
-        mInstance.setPhotoRes(R.drawable.img_appbar_top);
-        mInstance.setType(Constants.STATIC);
-
-        return mInstance;
-    }
-
-    public AppBarFragment() {
+    public AppBarBottomFragment() {
         // Required empty public constructor
     }
 
@@ -56,11 +45,11 @@ public class AppBarFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AppBarFragment.
+     * @return A new instance of fragment AppBarBottomFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AppBarFragment newInstance(String param1, String param2) {
-        AppBarFragment fragment = new AppBarFragment();
+    public static AppBarBottomFragment newInstance(String param1, String param2) {
+        AppBarBottomFragment fragment = new AppBarBottomFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -75,14 +64,16 @@ public class AppBarFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialog);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_app_bar, container, false);
+        View v = inflater.inflate(R.layout.fragment_app_bar_bottom, container, false);
         mUnbinder = ButterKnife.bind(this, v);
+
         return v;
     }
 
@@ -90,21 +81,5 @@ public class AppBarFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         mUnbinder.unbind();
-    }
-
-    @OnClick({R.id.btnTop,R.id.btnBottom})
-    public void onClicked(View view){
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-
-        switch (view.getId()) {
-            case R.id.btnTop:
-                AppBarTopFragment topFragment = new AppBarTopFragment();
-                topFragment.show(transaction, AppBarTopFragment.TAG);
-                break;
-            case R.id.btnBottom:
-                AppBarBottomFragment bottomFragment = new AppBarBottomFragment();
-                bottomFragment.show(transaction, AppBarBottomFragment.TAG);
-                break;
-        }
     }
 }
